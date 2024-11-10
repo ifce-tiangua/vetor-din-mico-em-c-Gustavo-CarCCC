0b734dc3f3b2f19f9e5164740cc4cf225d1061d6
Gustavo Carvalho <gustavo54535251@gmail.com>
2024-11-10 09:40:17 -0300

    2

--- main.c
+++ main.c
@@ -1,9 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(){
+    int qtd, i;
+    scanf("%d",&qtd);
 
-    //insira aqui seu código
-    printf("Nao implementado ainda...");
+    getchar();
+    
+    if(qtd == 0){
+        printf("[vazio]");
+        return 0;
+    }
 
+    int *vetor = (int*)malloc(qtd * sizeof(int));
+
+    for (i = 0; i < qtd; i++)
+    {
+        scanf("%d",&vetor[i]);
+    }
+
+    printf("[");
+
+    for (i = 0; i < qtd; i++)
+    {
+        if(i == qtd-1){
+            printf("%d",vetor[i]);
+        }else{
+            printf("%d, ",vetor[i]);
+        }
+    }
+
+    printf("]");
+    free(vetor);
     return 0;
 }
